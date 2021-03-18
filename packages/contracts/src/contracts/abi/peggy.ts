2ee9661b2a6d823d74a631ffc7803572a0f6a195
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2021-03-18 23:06:03 +0100

    chore: updated peggy abi

--- packages/contracts/src/contracts/abi/peggy.ts
+++ packages/contracts/src/contracts/abi/peggy.ts
@@ -2,66 +2,42 @@ import { AbiItem } from 'web3-utils'
 
 const abi = [
   {
+    anonymous: false,
     inputs: [
       {
+        indexed: false,
+        internalType: 'string',
+        name: '_cosmosDenom',
+        type: 'string',
+      },
+      {
+        indexed: true,
         internalType: 'address',
         name: '_tokenContract',
         type: 'address',
       },
+      { indexed: false, internalType: 'string', name: '_name', type: 'string' },
       {
-        internalType: 'address',
-        name: '_destination',
-        type: 'address',
+        indexed: false,
+        internalType: 'string',
+        name: '_symbol',
+        type: 'string',
       },
       {
+        indexed: false,
+        internalType: 'uint8',
+        name: '_decimals',
+        type: 'uint8',
+      },
+      {
+        indexed: false,
         internalType: 'uint256',
-        name: '_amount',
+        name: '_eventNonce',
         type: 'uint256',
       },
     ],
-    name: 'sendToCosmos',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'uint256',
-        name: 'nonce',
-        type: 'uint256',
-      },
-    ],
-    name: 'setNonce',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'bytes32',
-        name: '_peggyId',
-        type: 'bytes32',
-      },
-      {
-        internalType: 'uint256',
-        name: '_powerThreshold',
-        type: 'uint256',
-      },
-      {
-        internalType: 'address[]',
-        name: '_validators',
-        type: 'address[]',
-      },
-      {
-        internalType: 'uint256[]',
-        name: '_powers',
-        type: 'uint256[]',
-      },
-    ],
-    stateMutability: 'nonpayable',
-    type: 'constructor',
+    name: 'ERC20DeployedEvent',
+    type: 'event',
   },
   {
     anonymous: false,
@@ -80,9 +56,9 @@ const abi = [
       },
       {
         indexed: true,
-        internalType: 'address',
+        internalType: 'bytes32',
         name: '_destination',
-        type: 'address',
+        type: 'bytes32',
       },
       {
         indexed: false,
@@ -99,69 +75,6 @@ const abi = [
     ],
     name: 'SendToCosmosEvent',
     type: 'event',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'address[]',
-        name: '_currentValidators',
-        type: 'address[]',
-      },
-      {
-        internalType: 'uint256[]',
-        name: '_currentPowers',
-        type: 'uint256[]',
-      },
-      {
-        internalType: 'uint256',
-        name: '_currentValsetNonce',
-        type: 'uint256',
-      },
-      {
-        internalType: 'uint8[]',
-        name: '_v',
-        type: 'uint8[]',
-      },
-      {
-        internalType: 'bytes32[]',
-        name: '_r',
-        type: 'bytes32[]',
-      },
-      {
-        internalType: 'bytes32[]',
-        name: '_s',
-        type: 'bytes32[]',
-      },
-      {
-        internalType: 'uint256[]',
-        name: '_amounts',
-        type: 'uint256[]',
-      },
-      {
-        internalType: 'address[]',
-        name: '_destinations',
-        type: 'address[]',
-      },
-      {
-        internalType: 'uint256[]',
-        name: '_fees',
-        type: 'uint256[]',
-      },
-      {
-        internalType: 'uint256',
-        name: '_batchNonce',
-        type: 'uint256',
-      },
-      {
-        internalType: 'address',
-        name: '_tokenContract',
-        type: 'address',
-      },
-    ],
-    name: 'submitBatch',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function',
   },
   {
     anonymous: false,
@@ -187,59 +100,6 @@ const abi = [
     ],
     name: 'TransactionBatchExecutedEvent',
     type: 'event',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'address[]',
-        name: '_newValidators',
-        type: 'address[]',
-      },
-      {
-        internalType: 'uint256[]',
-        name: '_newPowers',
-        type: 'uint256[]',
-      },
-      {
-        internalType: 'uint256',
-        name: '_newValsetNonce',
-        type: 'uint256',
-      },
-      {
-        internalType: 'address[]',
-        name: '_currentValidators',
-        type: 'address[]',
-      },
-      {
-        internalType: 'uint256[]',
-        name: '_currentPowers',
-        type: 'uint256[]',
-      },
-      {
-        internalType: 'uint256',
-        name: '_currentValsetNonce',
-        type: 'uint256',
-      },
-      {
-        internalType: 'uint8[]',
-        name: '_v',
-        type: 'uint8[]',
-      },
-      {
-        internalType: 'bytes32[]',
-        name: '_r',
-        type: 'bytes32[]',
-      },
-      {
-        internalType: 'bytes32[]',
-        name: '_s',
-        type: 'bytes32[]',
-      },
-    ],
-    name: 'updateValset',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function',
   },
   {
     anonymous: false,
@@ -268,104 +128,94 @@ const abi = [
   },
   {
     inputs: [
-      {
-        internalType: 'address',
-        name: '_erc20Address',
-        type: 'address',
-      },
+      { internalType: 'string', name: '_cosmosDenom', type: 'string' },
+      { internalType: 'string', name: '_name', type: 'string' },
+      { internalType: 'string', name: '_symbol', type: 'string' },
+      { internalType: 'uint8', name: '_decimals', type: 'uint8' },
+    ],
+    name: 'deployERC20',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      { internalType: 'bytes32', name: '_peggyId', type: 'bytes32' },
+      { internalType: 'uint256', name: '_powerThreshold', type: 'uint256' },
+      { internalType: 'address[]', name: '_validators', type: 'address[]' },
+      { internalType: 'uint256[]', name: '_powers', type: 'uint256[]' },
+    ],
+    name: 'initialize',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      { internalType: 'address', name: '_erc20Address', type: 'address' },
     ],
     name: 'lastBatchNonce',
-    outputs: [
-      {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256',
-      },
-    ],
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
     stateMutability: 'view',
     type: 'function',
   },
   {
     inputs: [
-      {
-        internalType: 'address',
-        name: '',
-        type: 'address',
-      },
+      { internalType: 'address', name: '_tokenContract', type: 'address' },
+      { internalType: 'bytes32', name: '_destination', type: 'bytes32' },
+      { internalType: 'uint256', name: '_amount', type: 'uint256' },
     ],
+    name: 'sendToCosmos',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [{ internalType: 'bytes32', name: '', type: 'bytes32' }],
+    name: 'state_invalidationMapping',
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [{ internalType: 'address', name: '', type: 'address' }],
     name: 'state_lastBatchNonces',
-    outputs: [
-      {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256',
-      },
-    ],
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
     stateMutability: 'view',
     type: 'function',
   },
   {
     inputs: [],
     name: 'state_lastEventNonce',
-    outputs: [
-      {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256',
-      },
-    ],
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
     stateMutability: 'view',
     type: 'function',
   },
   {
     inputs: [],
     name: 'state_lastValsetCheckpoint',
-    outputs: [
-      {
-        internalType: 'bytes32',
-        name: '',
-        type: 'bytes32',
-      },
-    ],
+    outputs: [{ internalType: 'bytes32', name: '', type: 'bytes32' }],
     stateMutability: 'view',
     type: 'function',
   },
   {
     inputs: [],
     name: 'state_lastValsetNonce',
-    outputs: [
-      {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256',
-      },
-    ],
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
     stateMutability: 'view',
     type: 'function',
   },
   {
     inputs: [],
     name: 'state_peggyId',
-    outputs: [
-      {
-        internalType: 'bytes32',
-        name: '',
-        type: 'bytes32',
-      },
-    ],
+    outputs: [{ internalType: 'bytes32', name: '', type: 'bytes32' }],
     stateMutability: 'view',
     type: 'function',
   },
   {
     inputs: [],
     name: 'state_powerThreshold',
-    outputs: [
-      {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256',
-      },
-    ],
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
     stateMutability: 'view',
     type: 'function',
   },
@@ -376,68 +226,42 @@ const abi = [
         name: '_currentValidators',
         type: 'address[]',
       },
-      {
-        internalType: 'uint256[]',
-        name: '_currentPowers',
-        type: 'uint256[]',
-      },
-      {
-        internalType: 'uint8[]',
-        name: '_v',
-        type: 'uint8[]',
-      },
-      {
-        internalType: 'bytes32[]',
-        name: '_r',
-        type: 'bytes32[]',
-      },
-      {
-        internalType: 'bytes32[]',
-        name: '_s',
-        type: 'bytes32[]',
-      },
-      {
-        internalType: 'bytes32',
-        name: '_theHash',
-        type: 'bytes32',
-      },
-      {
-        internalType: 'uint256',
-        name: '_powerThreshold',
-        type: 'uint256',
-      },
+      { internalType: 'uint256[]', name: '_currentPowers', type: 'uint256[]' },
+      { internalType: 'uint256', name: '_currentValsetNonce', type: 'uint256' },
+      { internalType: 'uint8[]', name: '_v', type: 'uint8[]' },
+      { internalType: 'bytes32[]', name: '_r', type: 'bytes32[]' },
+      { internalType: 'bytes32[]', name: '_s', type: 'bytes32[]' },
+      { internalType: 'uint256[]', name: '_amounts', type: 'uint256[]' },
+      { internalType: 'address[]', name: '_destinations', type: 'address[]' },
+      { internalType: 'uint256[]', name: '_fees', type: 'uint256[]' },
+      { internalType: 'uint256', name: '_batchNonce', type: 'uint256' },
+      { internalType: 'address', name: '_tokenContract', type: 'address' },
+      { internalType: 'uint256', name: '_batchTimeout', type: 'uint256' },
     ],
-    name: 'testCheckValidatorSignatures',
+    name: 'submitBatch',
     outputs: [],
-    stateMutability: 'pure',
+    stateMutability: 'nonpayable',
     type: 'function',
   },
   {
     inputs: [
+      { internalType: 'address[]', name: '_newValidators', type: 'address[]' },
+      { internalType: 'uint256[]', name: '_newPowers', type: 'uint256[]' },
+      { internalType: 'uint256', name: '_newValsetNonce', type: 'uint256' },
       {
         internalType: 'address[]',
-        name: '_validators',
+        name: '_currentValidators',
         type: 'address[]',
       },
-      {
-        internalType: 'uint256[]',
-        name: '_powers',
-        type: 'uint256[]',
-      },
-      {
-        internalType: 'uint256',
-        name: '_valsetNonce',
-        type: 'uint256',
-      },
-      {
-        internalType: 'bytes32',
-        name: '_peggyId',
-        type: 'bytes32',
-      },
+      { internalType: 'uint256[]', name: '_currentPowers', type: 'uint256[]' },
+      { internalType: 'uint256', name: '_currentValsetNonce', type: 'uint256' },
+      { internalType: 'uint8[]', name: '_v', type: 'uint8[]' },
+      { internalType: 'bytes32[]', name: '_r', type: 'bytes32[]' },
+      { internalType: 'bytes32[]', name: '_s', type: 'bytes32[]' },
     ],
-    name: 'testMakeCheckpoint',
+    name: 'updateValset',
     outputs: [],
-    stateMutability: 'pure',
+    stateMutability: 'nonpayable',
     type: 'function',
   },
 ] as AbiItem[]
