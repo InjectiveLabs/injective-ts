43796a9fe4cb4419534aedb716c803bc59cac5c6
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2022-03-13 19:24:41 +0100

    feat: injective price service

--- packages/ui-common/src/token/coinGeckoService.ts
+++ packages/ui-common/src/token/coinGeckoService.ts
@@ -1,10 +1,12 @@
 import { CoinGeckoApi } from '@injectivelabs/token-utils'
-import { BigNumberInBase } from '@injectivelabs/utils'
+import { BigNumberInBase, HttpClient } from '@injectivelabs/utils'
 import { BaseService } from '../BaseService'
-import { ServiceOptions } from '../types'
+import { ServiceOptions, CoinPriceFromInjectiveService } from '../types'
 
 export class TokenCoinGeckoService extends BaseService {
   private coinGeckoApi: CoinGeckoApi
+
+  private httpClient: HttpClient
 
   constructor(
     options: ServiceOptions,
@@ -12,6 +14,74 @@ export class TokenCoinGeckoService extends BaseService {
   ) {
     super(options)
     this.coinGeckoApi = new CoinGeckoApi(coinGeckoOptions)
+    this.httpClient = new HttpClient(
+      'https://testnet.asset.injective.dev/asset-price/v1/coin',
+    )
+  }
+
+  async fetchUsdTokenPrice(coinId: string) {
+    if (!coinId) {
+      return 0
+    }
+
+    try {
+      try {
+        const priceInUsdFromInjectiveService =
+          await this.fetchUsdTokenPriceFromInjectiveService(coinId)
+
+        if (priceInUsdFromInjectiveService) {
+          return priceInUsdFromInjectiveService
+        }
+      } catch (e) {
+        //
+      }
+
+      const priceInUsdFromCoinGecko =
+        await this.fetchUsdTokenPriceFromCoinGecko(coinId)
+
+      if (priceInUsdFromCoinGecko) {
+        return priceInUsdFromCoinGecko
+      }
+
+      return 0
+    } catch (e: any) {
+      throw new Error(e.message)
+    }
+  }
+
+  async fetchUsdTokenPriceFromInjectiveService(coinId: string) {
+    if (!coinId) {
+      return 0
+    }
+
+    try {
+      const pricesResponse = (await this.httpClient.get('price', {
+        coinIds: coinId,
+        currency: 'usd',
+      })) as {
+        data: CoinPriceFromInjectiveService[]
+      }
+
+      if (pricesResponse.data.length === 0) {
+        return 0
+      }
+
+      const [priceResponse] = pricesResponse.data
+
+      if (!priceResponse) {
+        return 0
+      }
+
+      const { current_price: priceInUsd } = priceResponse
+
+      if (!priceInUsd) {
+        return 0
+      }
+
+      return new BigNumberInBase(priceInUsd).toNumber()
+    } catch (e: any) {
+      throw new Error(e.message)
+    }
   }
 
   async fetchUsdTokenPriceFromCoinGecko(coinId: string) {
--- packages/ui-common/src/token/types.ts
+++ packages/ui-common/src/token/types.ts
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 import { TokenMeta } from '@injectivelabs/token-metadata'
 import { UiBridgeTransaction } from '../bridge/types'
 
@@ -12,6 +13,33 @@ export interface GrpcTokenMeta {
   logo: string
   decimals: number
   updatedAt: number
+}
+
+export interface CoinPriceFromInjectiveService {
+  id: string
+  symbol: string
+  name: string
+  image: string
+  current_price: number
+  market_cap: number
+  market_cap_rank: number
+  total_volume: number
+  high_24h: number
+  low_24h: number
+  price_change_24h: number
+  price_change_percentage_24h: number
+  market_cap_change_24h: number
+  market_cap_change_percentage_24h: number
+  circulating_supply: number
+  total_supply: number
+  max_supply: number
+  ath: number
+  ath_change_percentage: number
+  ath_date: Date
+  atl: number
+  atl_change_percentage: number
+  atl_date: string
+  last_updated: Date
 }
 
 /**
