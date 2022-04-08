91943321289e2b5c0a151707677dd6e9d54508e2
thomasRalee <thomas.leera@gmail.com>
2022-04-08 20:06:26 +0800

    chore: update devnet peggy and injective contract address

--- packages/contracts/src/addresses.ts
+++ packages/contracts/src/addresses.ts
@@ -60,8 +60,8 @@ export const contractAddressesByNetwork = {
     injective: '0x96853aBD7e589D06b7dade1b9264f1a5c2d3176E',
   },
   [Network.Devnet]: {
-    peggy: '0xE768662CcDd434312b9097c2cF558B683B6b5dDa',
-    injective: '0x87aB3B4C8661e07D6372361211B96ed4Dc36B1B5',
+    peggy: '0x3c92F7779A7845d5eEf307aEF39066Ddba04A54b',
+    injective: '0x3d940951C2cdFc7091cb6064A41053FBFbD016EF',
   },
   [Network.Devnet1]: {
     peggy: '0x3A8634160d7577593CEEcDb2cc038A3437607318',
