6a4621a62ea4267c3df9de82b31798bfe1245c02
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2022-05-03 13:11:59 +0200

    feat: added object formatter

--- packages/sdk-ts/src/utils/helpers.ts
+++ packages/sdk-ts/src/utils/helpers.ts
@@ -1,1 +1,9 @@
 export const isServerSide = () => typeof window === 'undefined'
+
+export const objectToJson = (
+  object: Record<string, any>,
+  replacer = null,
+  indentation = 2,
+): string => {
+  return JSON.stringify(object, replacer, indentation)
+}
