999dbd3745c04f675594df5d324c5368fe6e32b3
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2022-05-03 13:16:32 +0200

    fix: protoObject function

--- packages/sdk-ts/src/utils/helpers.ts
+++ packages/sdk-ts/src/utils/helpers.ts
@@ -2,8 +2,30 @@ export const isServerSide = () => typeof window === 'undefined'
 
 export const objectToJson = (
   object: Record<string, any>,
-  replacer = null,
-  indentation = 2,
+  {
+    replacer = null,
+    indentation = 2,
+  }: {
+    replacer?: any
+    indentation?: number
+  },
 ): string => {
   return JSON.stringify(object, replacer, indentation)
+}
+
+export const protoObjectToJson = (
+  object: any,
+  {
+    replacer = null,
+    indentation = 2,
+  }: {
+    replacer?: any
+    indentation?: number
+  },
+): string => {
+  if (object.toObject !== undefined) {
+    return JSON.stringify(object.toObject(), replacer, indentation)
+  }
+
+  return objectToJson(object, { replacer, indentation })
 }
