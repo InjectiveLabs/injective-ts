3035548292d4e85797bfcaba7a98f70722856401
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2022-06-09 11:15:16 +0300

    feat: gas price server

--- packages/sdk-ui-ts/src/services/gas.ts
+++ packages/sdk-ui-ts/src/services/gas.ts
@@ -2,104 +2,6 @@ import { HttpClient, BigNumber, BigNumberInWei } from '@injectivelabs/utils'
 import { Network } from '@injectivelabs/networks'
 import { GWEI_IN_WEI, DEFAULT_GAS_PRICE } from '../constants'
 
-const isTestnet = (network: Network) => {
-  return [
-    Network.Local,
-    Network.Devnet,
-    Network.Devnet1,
-    Network.Testnet,
-    Network.TestnetK8s,
-  ].includes(network)
-}
-
-const fetchGasPriceFromOwlracle = async (): Promise<string> => {
-  try {
-    const response = (await new HttpClient('https://owlracle.info/eth').get(
-      'gas',
-    )) as {
-      data: OwlracleResult
-    }
-
-    if (!response || (response && !response.data)) {
-      throw new Error('No response from Owrlacle')
-    }
-    const { speeds } = response.data
-    // @ts-ignore
-    const [slow, fast, faster, fastest] = speeds
-
-    return new BigNumberInWei(
-      new BigNumber(faster.gasPrice).multipliedBy(GWEI_IN_WEI),
-    ).toString()
-  } catch (e: any) {
-    throw new Error(e.message)
-  }
-}
-
-const fetchGasPriceFromEtherchain = async (): Promise<string> => {
-  try {
-    const response = (await new HttpClient(
-      'https://www.etherchain.org/api/',
-    ).get('gasPriceOracle')) as {
-      data: EtherchainResult
-    }
-
-    if (!response || (response && !response.data)) {
-      throw new Error('No response from Etherchain')
-    }
-
-    return new BigNumberInWei(
-      new BigNumber(
-        response.data.currentBaseFee * response.data.fast,
-      ).multipliedBy(GWEI_IN_WEI),
-    ).toString()
-  } catch (e: any) {
-    throw new Error(e.message)
-  }
-}
-
-const fetchGasPriceFromEthGasStation = async (): Promise<string> => {
-  try {
-    const response = (await new HttpClient(
-      'https://ethgasstation.info/json',
-    ).get('ethgasAPI.json')) as {
-      data: EthGasStationResult
-    }
-
-    if (!response || (response && !response.data)) {
-      throw new Error('No response from Ethgasstation')
-    }
-
-    return new BigNumberInWei(
-      new BigNumber(response.data.fastest / 10).multipliedBy(GWEI_IN_WEI),
-    ).toString()
-  } catch (e: any) {
-    throw new Error(e.message)
-  }
-}
-
-// @ts-ignore
-const fetchGasPriceFromMetamaskGasServer = async (): Promise<string> => {
-  try {
-    const response = (await new HttpClient(
-      'https://mock-gas-server.herokuapp.com',
-    ).get('')) as {
-      data: MetamaskGasServerResult
-    }
-
-    if (!response || (response && !response.data)) {
-      throw new Error('No response from Metamask')
-    }
-
-    return new BigNumberInWei(
-      new BigNumber(response.data.medium.suggestedMaxFeePerGas)
-        .div(1000)
-        .multipliedBy(GWEI_IN_WEI),
-    ).toString()
-  } catch (e: any) {
-    throw new Error(e.message)
-  }
-}
-
 export interface MetamaskGasServerResult {
   low: {
     minWaitTimeEstimate: number
@@ -182,13 +84,111 @@ export interface EtherchainResult {
   recommendedBaseFee: number
 }
 
+const isTestnet = (network: Network) => {
+  return [
+    Network.Local,
+    Network.Devnet,
+    Network.Devnet1,
+    Network.Testnet,
+    Network.TestnetK8s,
+  ].includes(network)
+}
+
+const fetchGasPriceFromOwlracle = async (): Promise<string> => {
+  try {
+    const response = (await new HttpClient('https://owlracle.info/eth').get(
+      'gas',
+    )) as {
+      data: OwlracleResult
+    }
+
+    if (!response || (response && !response.data)) {
+      throw new Error('No response from Owrlacle')
+    }
+    const { speeds } = response.data
+    // @ts-ignore
+    const [slow, fast, faster, fastest] = speeds
+
+    return new BigNumberInWei(
+      new BigNumber(faster.gasPrice).multipliedBy(GWEI_IN_WEI),
+    ).toString()
+  } catch (e: any) {
+    throw new Error(e.message)
+  }
+}
+
+const fetchGasPriceFromEtherchain = async (): Promise<string> => {
+  try {
+    const response = (await new HttpClient(
+      'https://www.etherchain.org/api/',
+    ).get('gasPriceOracle')) as {
+      data: EtherchainResult
+    }
+
+    if (!response || (response && !response.data)) {
+      throw new Error('No response from Etherchain')
+    }
+
+    return new BigNumberInWei(
+      new BigNumber(
+        response.data.currentBaseFee * response.data.fast,
+      ).multipliedBy(GWEI_IN_WEI),
+    ).toString()
+  } catch (e: any) {
+    throw new Error(e.message)
+  }
+}
+
+const fetchGasPriceFromEthGasStation = async (): Promise<string> => {
+  try {
+    const response = (await new HttpClient(
+      'https://ethgasstation.info/json',
+    ).get('ethgasAPI.json')) as {
+      data: EthGasStationResult
+    }
+
+    if (!response || (response && !response.data)) {
+      throw new Error('No response from Ethgasstation')
+    }
+
+    return new BigNumberInWei(
+      new BigNumber(response.data.fastest / 10).multipliedBy(GWEI_IN_WEI),
+    ).toString()
+  } catch (e: any) {
+    throw new Error(e.message)
+  }
+}
+
+// @ts-ignore
+const fetchGasPriceFromMetaswapGasServer = async (): Promise<string> => {
+  try {
+    const response = (await new HttpClient(
+      'https://gas-api.metaswap.codefi.network/networks/1/suggestedGasFees',
+    ).get('')) as {
+      data: MetamaskGasServerResult
+    }
+
+    if (!response || (response && !response.data)) {
+      throw new Error('No response from Metamask')
+    }
+
+    return new BigNumberInWei(
+      new BigNumber(response.data.medium.suggestedMaxFeePerGas)
+        .div(1000)
+        .multipliedBy(GWEI_IN_WEI),
+    ).toString()
+  } catch (e: any) {
+    throw new Error(e.message)
+  }
+}
+
 export const fetchGasPrice = async (network: Network): Promise<string> => {
   if (isTestnet(network)) {
     return new BigNumberInWei(DEFAULT_GAS_PRICE).toString()
   }
 
   try {
-    return await fetchGasPriceFromOwlracle()
+    return await fetchGasPriceFromMetaswapGasServer()
   } catch (e) {
     //
   }
@@ -201,6 +201,12 @@ export const fetchGasPrice = async (network: Network): Promise<string> => {
 
   try {
     return await fetchGasPriceFromEtherchain()
+  } catch (e) {
+    //
+  }
+
+  try {
+    return await fetchGasPriceFromOwlracle()
   } catch (e) {
     //
   }
