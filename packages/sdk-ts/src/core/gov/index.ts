a56004fc8cb8f4a933956960e4b7cb9897f698c4
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2022-06-15 23:37:59 +0300

    fix: export

--- packages/sdk-ts/src/core/gov/index.ts
+++ packages/sdk-ts/src/core/gov/index.ts
@@ -1,9 +1,24 @@
 export { default as MsgVote } from './msgs/MsgVote'
 export { default as MsgGovDeposit } from './msgs/MsgDeposit'
-export { default as MsgSubmitProposalExpiryFuturesMarketLaunch } from './msgs/MsgSubmitProposalExpiryFuturesMarketLaunch'
-export { default as MsgSubmitProposalPerpetualMarketLaunch } from './msgs/MsgSubmitProposalPerpetualMarketLaunch'
-export { default as MsgSubmitProposalSpotMarketLaunch } from './msgs/MsgSubmitProposalSpotMarketLaunch'
-export { default as MsgSubmitProposalSpotMarketParamUpdate } from './msgs/MsgSubmitProposalSpotMarketParamUpdate'
-export { default as MsgSubmitTextProposal } from './msgs/MsgSubmitTextProposal'
+import { default as MsgSubmitProposalExpiryFuturesMarketLaunch } from './msgs/MsgSubmitProposalExpiryFuturesMarketLaunch'
+import { default as MsgSubmitProposalPerpetualMarketLaunch } from './msgs/MsgSubmitProposalPerpetualMarketLaunch'
+import { default as MsgSubmitProposalSpotMarketLaunch } from './msgs/MsgSubmitProposalSpotMarketLaunch'
+import { default as MsgSubmitProposalSpotMarketParamUpdate } from './msgs/MsgSubmitProposalSpotMarketParamUpdate'
+import { default as MsgSubmitTextProposal } from './msgs/MsgSubmitTextProposal'
+
+export type MsgSubmitProposal =
+  | MsgSubmitProposalExpiryFuturesMarketLaunch
+  | MsgSubmitProposalPerpetualMarketLaunch
+  | MsgSubmitProposalSpotMarketLaunch
+  | MsgSubmitProposalSpotMarketParamUpdate
+  | MsgSubmitTextProposal
+
+export {
+  MsgSubmitProposalExpiryFuturesMarketLaunch,
+  MsgSubmitProposalPerpetualMarketLaunch,
+  MsgSubmitProposalSpotMarketLaunch,
+  MsgSubmitProposalSpotMarketParamUpdate,
+  MsgSubmitTextProposal,
+}
 
 export * from './ProposalContentDecomposer'
