be3645e9c7504eaad89ec54538a06d6d83e99dc4
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2022-06-20 20:30:54 +0200

    fix: binary options types

--- packages/sdk-ts/src/client/exchange/types/binary-options.ts
+++ packages/sdk-ts/src/client/exchange/types/binary-options.ts
@@ -1,33 +1,5 @@
-import {
-  DerivativeLimitOrder as GrpcBinaryOptionsLimitOrder,
-  DerivativeTrade as GrpcBinaryOptionsTrade,
-  DerivativePosition as GrpcBinaryOptionsPosition,
-} from '@injectivelabs/exchange-api/injective_derivative_exchange_rpc_pb'
 import { BinaryOptionsMarketInfo as GrpcBinaryOptionsMarketInfo } from '@injectivelabs/exchange-api/injective_binary_options_exchange_rpc_pb'
-import { TradeExecutionType, TradeDirection } from '@injectivelabs/ts-types'
-import { GrpcOrderType } from '../../chain/types/exchange'
 import { TokenMeta } from '@injectivelabs/token-metadata'
-import { PositionDelta } from './derivatives'
-
-export enum BinaryOptionsOrderSide {
-  Unspecified = 'unspecified',
-  Buy = 'buy',
-  Sell = 'sell',
-  StopBuy = 'stop_buy',
-  StopSell = 'stop_sell',
-  TakeBuy = 'take_buy',
-  TakeSell = 'take_sell',
-  BuyPO = 'buy_po',
-  SellPO = 'sell_po',
-}
-
-export enum BinaryOptionsOrderState {
-  Unfilled = 'unfilled',
-  Booked = 'booked',
-  PartialFilled = 'partial_filled',
-  Filled = 'filled',
-  Canceled = 'canceled',
-}
 
 export interface BinaryOptionsMarket {
   marketId: string
@@ -49,56 +21,4 @@ export interface BinaryOptionsMarket {
   settlementPrice: string
 }
 
-export interface BinaryOptionsLimitOrder {
-  orderHash: string
-  orderSide: BinaryOptionsOrderSide
-  marketId: string
-  subaccountId: string
-  isReduceOnly: boolean
-  margin: string
-  price: string
-  quantity: string
-  unfilledQuantity: string
-  triggerPrice: string
-  feeRecipient: string
-  state: BinaryOptionsOrderState
-}
-
-export interface BinaryOptionsTrade extends PositionDelta {
-  orderHash: string
-  subaccountId: string
-  marketId: string
-  executedAt: number
-  tradeExecutionType: TradeExecutionType
-  tradeDirection: TradeDirection
-  fee: string
-  feeRecipient: string
-  isLiquidation: boolean
-  payout: string
-}
-
-export interface BinaryOptionsLimitOrderParams {
-  orderType: GrpcOrderType
-  triggerPrice?: string
-  feeRecipient: string
-  price: string
-  margin: string
-  quantity: string
-}
-
-export interface BinaryOptionsOrderCancelParams {
-  orderHash: string
-}
-
-export interface BatchBinaryOptionsOrderCancelParams {
-  marketId: string
-  subaccountId: string
-  orderHash: string
-}
-
-export {
-  GrpcBinaryOptionsPosition,
-  GrpcBinaryOptionsMarketInfo,
-  GrpcBinaryOptionsLimitOrder,
-  GrpcBinaryOptionsTrade,
-}
+export { GrpcBinaryOptionsMarketInfo }
--- packages/sdk-ui-ts/src/client/transformers/UiBinaryOptionsTransformer.ts
+++ packages/sdk-ui-ts/src/client/transformers/UiBinaryOptionsTransformer.ts
@@ -1,19 +1,15 @@
-import { BigNumber, BigNumberInBase } from '@injectivelabs/utils'
-import { MarketType, Change } from '../types/common'
+import { BigNumberInBase } from '@injectivelabs/utils'
+import { MarketType } from '../types/common'
 import { getDecimalsFromNumber } from '@injectivelabs/sdk-ts'
 import {
-  UiBaseDerivativeMarketWithToken,
-  UiDerivativeMarketSummary,
-  UiDerivativeMarketWithToken,
-} from '../types/derivatives'
-import { derivativeOrderTypeToGrpcOrderType } from '../../utils/derivatives'
+  UiBaseBinaryOptionsMarketWithToken,
+  UiBinaryOptionsMarketWithToken,
+} from '../types/binary-options'
 
-export class UiDerivativeTransformer {
-  static derivativeOrderTypeToGrpcOrderType = derivativeOrderTypeToGrpcOrderType
-
-  static derivativeMarketToUiDerivativeMarket(
-    market: UiBaseDerivativeMarketWithToken,
-  ): UiDerivativeMarketWithToken {
+export class UiBinaryOptionsTransformer {
+  static binaryOptionsMarketToUiBinaryOptionsMarket(
+    market: UiBaseBinaryOptionsMarketWithToken,
+  ): UiBinaryOptionsMarketWithToken {
     return {
       ...market,
       type: MarketType.Derivative,
@@ -27,78 +23,11 @@ export class UiDerivativeTransformer {
     }
   }
 
-  static derivativeMarketSummaryToUiMarketSummary(
-    oldSummary: UiDerivativeMarketSummary,
-    newSummary: UiDerivativeMarketSummary,
-  ): UiDerivativeMarketSummary {
-    if (new BigNumber(oldSummary.price).eq(newSummary.price)) {
-      return {
-        ...newSummary,
-        lastPrice: oldSummary.price,
-        lastPriceChange: oldSummary.lastPriceChange || Change.NoChange,
-      }
-    }
-
-    return {
-      ...newSummary,
-      lastPrice: oldSummary.price,
-      lastPriceChange: new BigNumber(newSummary.price).gte(oldSummary.price)
-        ? Change.Increase
-        : Change.Decrease,
-    }
-  }
-
-  static derivativeMarketsSummaryToUiMarketsSummary(
-    oldSummaries: UiDerivativeMarketSummary[] = [],
-    newSummaries: UiDerivativeMarketSummary[] = [],
-  ): UiDerivativeMarketSummary[] {
-    return oldSummaries.map((oldSummary) => {
-      const newSummary = newSummaries.find(
-        (m) => m.marketId === oldSummary.marketId,
-      )
-
-      // Sometimes, chronos returns zeros
-      const actualNewSummary =
-        newSummary && newSummary.price ? newSummary : oldSummary
-
-      return UiDerivativeTransformer.derivativeMarketSummaryToUiMarketSummary(
-        oldSummary,
-        actualNewSummary,
-      )
-    })
-  }
-
-  static derivativeMarketsSummaryComparisons(
-    newMarketSummary?: UiDerivativeMarketSummary[],
-    oldMarketsSummary?: UiDerivativeMarketSummary[],
-  ) {
-    if (!oldMarketsSummary && !newMarketSummary) {
-      return undefined
-    }
-
-    if (!newMarketSummary) {
-      return oldMarketsSummary as UiDerivativeMarketSummary[]
-    }
-
-    if (!oldMarketsSummary) {
-      return newMarketSummary
-    }
-
-    const marketsWithOldSummaries = oldMarketsSummary.filter((market) =>
-      newMarketSummary.find((m) => m.marketId === market.marketId),
-    )
-
-    return UiDerivativeTransformer.derivativeMarketsSummaryToUiMarketsSummary(
-      marketsWithOldSummaries,
-      newMarketSummary,
-    )
-  }
-
-  static derivativeMarketsToUiSpotMarkets(
-    markets: UiBaseDerivativeMarketWithToken[],
+  static binaryOptionsMarketsToUiBinaryOptionsMarkets(
+    markets: UiBaseBinaryOptionsMarketWithToken[],
   ) {
     return markets.map(
-      UiDerivativeTransformer.derivativeMarketToUiDerivativeMarket,
+      UiBinaryOptionsTransformer.binaryOptionsMarketToUiBinaryOptionsMarket,
     )
   }
 }
