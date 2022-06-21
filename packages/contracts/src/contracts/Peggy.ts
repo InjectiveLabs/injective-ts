54a9ae772e0fccd2ec23e364237f73613316ec1c
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2022-06-21 17:25:20 +0200

    fix: old peggy contract

--- packages/contracts/src/contracts/Peggy.ts
+++ packages/contracts/src/contracts/Peggy.ts
@@ -34,7 +34,7 @@ export class PeggyContract extends BaseContract<any> {
     contractAddress,
     address,
     amount,
-    data,
+    data = '',
     transactionOptions,
   }: {
     contractAddress: string
