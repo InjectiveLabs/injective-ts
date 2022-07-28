eaff89b73d975473c05fe7722d646038daff1486
Shane K Moore <41407272+shane-moore@users.noreply.github.com>
2022-07-28 08:32:28 -0500

    chore: create getTokenLogoFromTokenType helper method

--- packages/sdk-ui-ts/src/utils/helpers.ts
+++ packages/sdk-ui-ts/src/utils/helpers.ts
@@ -1,4 +1,5 @@
 import path from 'path'
+import { TokenType, IbcToken, Token } from '@injectivelabs/token-metadata'
 
 export const getTokenLogoWithVendorPathPrefix = (image: string) => {
   if (image.includes('@injectivelabs')) {
@@ -14,4 +15,30 @@ export const getTokenLogoWithVendorPathPrefix = (image: string) => {
   }
 
   return path.join('/', 'vendor', '@injectivelabs', 'token-metadata', image)
+}
+
+export const getTokenLogoFromTokenType = (token: Token) => {
+  const { tokenType, logo } = token
+
+  const { isCanonical } = token as IbcToken
+
+  const isIbcOrEthTokenWithVendorLogo =
+    logo && logo !== '' && logo !== TokenType.Cw20
+
+  switch (true) {
+    case tokenType === TokenType.InsuranceFund:
+      return getTokenLogoWithVendorPathPrefix('insurance-fund.svg')
+    case tokenType === TokenType.Native:
+      return getTokenLogoWithVendorPathPrefix('injective-black-fill.svg')
+    case tokenType === TokenType.Cw20 && !logo:
+      return getTokenLogoWithVendorPathPrefix('cw20.svg')
+    case tokenType === TokenType.Cw20 && logo:
+      return logo
+    case tokenType === TokenType.Ibc && !isCanonical:
+      return getTokenLogoWithVendorPathPrefix('ibc.svg')
+    case isIbcOrEthTokenWithVendorLogo:
+      return getTokenLogoWithVendorPathPrefix(logo)
+    default:
+      return getTokenLogoWithVendorPathPrefix('injective-black-fill.svg')
+  }
 }
