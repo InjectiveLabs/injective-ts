7559a3c82bd0acc427d3c0528e1ae8b8ef28cced
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2022-07-30 13:01:27 +0200

    fix: bignumber conversion

--- packages/utils/src/classes/BigNumber/utils.ts
+++ packages/utils/src/classes/BigNumber/utils.ts
@@ -4,7 +4,7 @@ export const bigNumberBaseToWei = <T extends string | number | BigNumber, P>(
   value: T,
   decimals = 18,
 ): P =>
-  new BigNumber(value).dividedBy(
+  new BigNumber(value).multipliedBy(
     new BigNumber(10).pow(decimals),
   ) as unknown as P
 
@@ -12,6 +12,6 @@ export const bigNumberWeiToBase = <T extends string | number | BigNumber, P>(
   value: T,
   decimals = 18,
 ): P =>
-  new BigNumber(value).multipliedBy(
+  new BigNumber(value).dividedBy(
     new BigNumber(10).pow(decimals),
   ) as unknown as P
