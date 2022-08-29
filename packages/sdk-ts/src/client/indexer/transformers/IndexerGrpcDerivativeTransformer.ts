de736de2a2505b9874eacc3cd1403119f6628c64
Kevin Karsopawiro <k.karsopawiro@gmail.com>
2022-08-29 10:48:36 +0200

    feat: order history

--- packages/sdk-ts/src/client/indexer/transformers/IndexerGrpcDerivativeTransformer.ts
+++ packages/sdk-ts/src/client/indexer/transformers/IndexerGrpcDerivativeTransformer.ts
@@ -444,6 +444,7 @@ export class IndexerGrpcDerivativeTransformer {
       isConditional: orderHistory.getIsConditional(),
       triggerAt: orderHistory.getTriggerAt(),
       placedOrderHash: orderHistory.getPlacedOrderHash(),
+      margin: orderHistory.getMargin()
     }
   }
 
