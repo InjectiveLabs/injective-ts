0a125e0d1297b0ce8095f90e9d053b8e84462b0b
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2022-09-07 21:37:32 +0200

    feat: added sync mode to TxGrpcClient

--- packages/utils/src/helpers.ts
+++ packages/utils/src/helpers.ts
@@ -1,19 +1,9 @@
-import { ComposerResponse, Web3GatewayMessage } from '@injectivelabs/ts-types'
-import snakeCaseKeys from 'snakecase-keys'
+import { ComposerResponse } from '@injectivelabs/ts-types'
 
 export const sleep = (timeout: number): Promise<void> =>
   new Promise((resolve) => setTimeout(resolve, timeout))
 
 export const isServerSide = () => typeof window === 'undefined'
-
-export const getWeb3GatewayMessage = <T>(
-  message: T,
-  type: string,
-): Web3GatewayMessage<T> =>
-  ({
-    ...snakeCaseKeys(message),
-    '@type': type,
-  } as Web3GatewayMessage<T>)
 
 export const mapMultipleComposerResponseMessages = <T, R>(
   messages: ComposerResponse<T, R>[],
