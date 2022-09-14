cb98d2c6b42fa612ba4e7b976f537341ade5ea3d
thomasRalee <thomas.leera@gmail.com>
2022-09-14 23:34:47 +0800

    feat: add unit test coverage for injective exchange MsgIncreasePositionMargin message

---
+++ packages/sdk-ts/src/core/modules/exchange/msgs/MsgIncreasePositionMargin.spec.ts
@@ -0,0 +1,90 @@
+import { MsgIncreasePositionMargin as BaseMsgIncreasePositionMargin } from '@injectivelabs/chain-api/injective/exchange/v1beta1/tx_pb'
+import MsgIncreasePositionMargin from './MsgIncreasePositionMargin'
+import { mockFactory } from '../../../../../../../tests/mocks'
+import snakecaseKeys from 'snakecase-keys'
+
+const params: MsgIncreasePositionMargin['params'] = {
+  marketId: mockFactory.injUsdtDerivativeMarket.marketId,
+  injectiveAddress: mockFactory.injectiveAddress,
+  srcSubaccountId: mockFactory.subaccountId,
+  dstSubaccountId: mockFactory.subaccountId2,
+  amount: '1000',
+}
+
+const protoType = '/injective.exchange.v1beta1.MsgIncreasePositionMargin'
+const protoTypeShort = 'exchange/MsgIncreasePositionMargin'
+const protoParams = {
+  marketId: params.marketId,
+  sender: params.injectiveAddress,
+  sourceSubaccountId: params.srcSubaccountId,
+  destinationSubaccountId: params.dstSubaccountId,
+  amount: params.amount,
+}
+
+const message = MsgIncreasePositionMargin.fromJSON(params)
+
+describe.only('MsgIncreasePositionMargin', () => {
+  it('generates proper proto', () => {
+    const proto = message.toProto()
+
+    expect(proto instanceof BaseMsgIncreasePositionMargin).toBe(true)
+    expect(proto.toObject()).toStrictEqual({
+      ...protoParams,
+      amount: '1000000000000000000000',
+    })
+  })
+
+  it('generates proper data', () => {
+    const data = message.toData()
+
+    expect(data).toStrictEqual({
+      '@type': protoType,
+      ...protoParams,
+      amount: '1000000000000000000000',
+    })
+  })
+
+  it('generates proper amino', () => {
+    const amino = message.toAmino()
+
+    expect(amino).toStrictEqual({
+      type: protoTypeShort,
+      ...protoParams,
+    })
+  })
+
+  it('generates proper Eip712 types', () => {
+    const eip712Types = message.toEip712Types()
+
+    expect(Object.fromEntries(eip712Types)).toStrictEqual({
+      MsgValue: [
+        { name: 'sender', type: 'string' },
+        { name: 'source_subaccount_id', type: 'string' },
+        { name: 'destination_subaccount_id', type: 'string' },
+        { name: 'market_id', type: 'string' },
+        { name: 'amount', type: 'string' },
+      ],
+    })
+  })
+
+  it('generates proper Eip712 values', () => {
+    const eip712 = message.toEip712()
+
+    expect(eip712).toStrictEqual({
+      type: protoTypeShort,
+      value: snakecaseKeys({
+        ...protoParams,
+        amount: '1000.000000000000000000',
+      }),
+    })
+  })
+
+  it('generates proper web3', () => {
+    const web3 = message.toWeb3()
+
+    expect(web3).toStrictEqual({
+      '@type': protoType,
+      ...protoParams,
+    })
+  })
+})
--- tests/mocks/index.ts
+++ tests/mocks/index.ts
@@ -8,6 +8,8 @@ const derivativeMarketId =
   '0x7cc8b10d7deb61e744ef83bdec2bbcf4a056867e89b062c6a453020ca82bd4e4'
 const subaccountId =
   '0xaf79152ac5df276d9a8e1e2e22822f9713474902000000000000000000000000'
+const subaccountId2 =
+  '0xaf79152ac5df276d9a8e1e2e22822f9713474902000000000000000000000001'
 
 export const injUsdtSpotMarket = {
   marketId:
@@ -77,6 +79,7 @@ export const mockFactory = {
   derivativeMarketId,
   spotMarketId,
   subaccountId,
+  subaccountId2,
   injUsdtSpotMarket,
   injUsdtDerivativeMarket,
 }
