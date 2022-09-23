c94bcc6efe81419eddf32a4ad7dde687bc696042
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2022-09-23 08:49:56 +0200

    fix: web3 instance

--- packages/wallet-ts/src/wallet-strategy/strategies/Base.ts
+++ packages/wallet-ts/src/wallet-strategy/strategies/Base.ts
@@ -16,11 +16,13 @@ export default abstract class BaseConcreteStrategy {
   protected constructor({
     ethereumChainId,
     chainId,
+    web3,
   }: {
     ethereumChainId: EthereumChainId
     chainId: ChainId
     web3?: Web3
   }) {
+    this.web3 = web3
     this.ethereumChainId = ethereumChainId
     this.chainId = chainId
   }
