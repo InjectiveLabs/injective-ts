20b78e7fc3101799e7ebd049cbbfd40874e4218a
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2022-09-26 18:55:07 +0200

    fix: order of conditionals

--- packages/sdk-ts/src/utils/grpc.ts
+++ packages/sdk-ts/src/utils/grpc.ts
@@ -1,19 +1,15 @@
 import { grpc } from '@improbable-eng/grpc-web'
 import { NodeHttpTransport } from '@improbable-eng/grpc-web-node-http-transport'
 import { ReactNativeTransport } from '@improbable-eng/grpc-web-react-native-transport'
-import { isBrowser, isNode, isReactNative } from './helpers'
+import { isNode, isReactNative } from './helpers'
 
 export const getGrpcTransport = (): grpc.TransportFactory | undefined => {
-  if (isBrowser()) {
-    return undefined
+  if (isReactNative()) {
+    return ReactNativeTransport({ withCredentials: true })
   }
 
   if (isNode()) {
     return NodeHttpTransport()
-  }
-
-  if (isReactNative()) {
-    return ReactNativeTransport({ withCredentials: true })
   }
 
   return undefined
