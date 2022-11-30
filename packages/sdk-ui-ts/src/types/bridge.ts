fee3a8007c735772f8ba9556f3a3b6fe5291fedc
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2022-11-30 15:31:05 +0100

    feat: added bridge transaction type

--- packages/sdk-ui-ts/src/types/bridge.ts
+++ packages/sdk-ui-ts/src/types/bridge.ts
@@ -82,6 +82,7 @@ export interface UiBridgeTransaction {
   nonce?: number
   bridgeFee?: string
   timeoutTimestamp?: string
+  type: `${BridgingNetwork}-${BridgingNetwork}`
   txHashes?: string[]
 }
 
