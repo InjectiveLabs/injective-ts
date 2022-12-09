f354be990c04da62cbe95e49bc7c19fc2c34d91b
thomasRalee <thomas.leera@gmail.com>
2022-12-09 23:36:13 +0800

    chore: ExecPrivilegedArgVaultSubscribe add subscriptionType params

--- packages/sdk-ts/src/core/modules/wasm/exec-priv-args/ExecPrivilegedArgVaultSubscribe.ts
+++ packages/sdk-ts/src/core/modules/wasm/exec-priv-args/ExecPrivilegedArgVaultSubscribe.ts
@@ -8,14 +8,14 @@ export declare namespace ExecPrivilegedArgVaultSubscribe {
   export interface Params {
     vaultSubaccountId: string
     subscriberSubaccountId: string
-    marginRatio?: string
     origin: string
+    subscriptionType: Record<string, any>
   }
 
   export interface Data {
     vault_subaccount_id: string
     subscriber_subaccount_id: string
-    margin_ratio?: string
+    subscription_type: Record<string, any>
   }
 }
 
@@ -38,7 +38,7 @@ export default class ExecPrivilegedArgVaultSubscribe extends ExecPrivilegedArgBa
     return {
       vault_subaccount_id: params.vaultSubaccountId,
       subscriber_subaccount_id: params.subscriberSubaccountId,
-      margin_ratio: params.marginRatio,
+      subscription_type: params.subscriptionType,
     }
   }
 
