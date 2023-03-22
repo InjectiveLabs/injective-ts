51b4e4bc2012f60cfb50d9bbed6bbed7bd8a9283
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2023-03-22 03:43:18 +0100

    chore: minor test

--- packages/token-metadata/test/unit/cw20.spec.ts
+++ packages/token-metadata/test/unit/cw20.spec.ts
@@ -10,7 +10,7 @@ describe('token meta retrieved properly for', () => {
 
     const tokenMeta = tokenFactory.toToken(LUNADenom) as Token
 
-    expect(tokenMeta.ibc).toStrictEqual({
+    expect(tokenMeta.ibc).toEqual({
       decimals: 6,
       isNative: true,
       baseDenom: 'uluna',
@@ -24,7 +24,7 @@ describe('token meta retrieved properly for', () => {
   describe('chz token', () => {
     const CHZFactoryDenom =
       'factory/inj14ejqjyq8um4p3xfqj74yld5waqljf88f9eneuk/inj1q6kpxy6ar5lkxqudjvryarrrttmakwsvzkvcyh'
-    const CHZPeggyDenom = 'peggy0x3506424f91fd33084466f402d5d97f05f8e3b4af'
+    const CHZPeggyDenom = 'peggy0x3506424F91fD33084466F402d5D97f05F8e3b4AF'
 
     it('fetches correct decimal value for cw20 format', () => {
       const tokenMeta = tokenFactory.toToken(CHZFactoryDenom) as Token
@@ -44,18 +44,20 @@ describe('token meta retrieved properly for', () => {
       'factory/inj14ejqjyq8um4p3xfqj74yld5waqljf88f9eneuk/inj1q6zlut7gtkzknkk773jecujwsdkgq882akqksk'
     const USDCsoFactoryDenom =
       'factory/inj14ejqjyq8um4p3xfqj74yld5waqljf88f9eneuk/inj12pwnhtv7yat2s30xuf4gdk9qm85v4j3e60dgvu'
-    const USDCPeggyDenom = 'peggy0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
+    const USDCPeggyDenom = 'peggy0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
 
     it('fetches correct value for peggyUsdc', () => {
       const tokenMeta = tokenFactory.toToken(USDCetFactoryDenom) as Token
 
-      expect(tokenMeta).toStrictEqual(
+      expect(tokenMeta).toEqual(
         expect.objectContaining({
-          decimals: 6,
-          symbol: 'USDCet',
-          source: 'ethereum-wormhole',
-          address: 'inj1q6zlut7gtkzknkk773jecujwsdkgq882akqksk',
-          tokenType: 'cw20',
+          cw20: {
+            decimals: 6,
+            symbol: 'USDCet',
+            source: 'ethereum-wormhole',
+            address: 'inj1q6zlut7gtkzknkk773jecujwsdkgq882akqksk',
+            tokenType: 'cw20',
+          },
         }),
       )
     })
@@ -63,13 +65,15 @@ describe('token meta retrieved properly for', () => {
     it('fetches correct value for Usdcet', () => {
       const tokenMeta = tokenFactory.toToken(USDCetFactoryDenom) as Token
 
-      expect(tokenMeta).toStrictEqual(
+      expect(tokenMeta).toEqual(
         expect.objectContaining({
-          decimals: 6,
-          symbol: 'USDCet',
-          source: 'ethereum-wormhole',
-          address: 'inj1q6zlut7gtkzknkk773jecujwsdkgq882akqksk',
-          tokenType: 'cw20',
+          cw20: {
+            decimals: 6,
+            symbol: 'USDCet',
+            source: 'ethereum-wormhole',
+            address: 'inj1q6zlut7gtkzknkk773jecujwsdkgq882akqksk',
+            tokenType: 'cw20',
+          },
         }),
       )
     })
@@ -77,13 +81,15 @@ describe('token meta retrieved properly for', () => {
     it('fetches correct value for Usdcso', () => {
       const tokenMeta = tokenFactory.toToken(USDCsoFactoryDenom) as Token
 
-      expect(tokenMeta).toStrictEqual(
+      expect(tokenMeta).toEqual(
         expect.objectContaining({
-          decimals: 6,
-          symbol: 'USDCso',
-          source: 'solana',
-          address: 'inj12pwnhtv7yat2s30xuf4gdk9qm85v4j3e60dgvu',
-          tokenType: 'cw20',
+          cw20: {
+            decimals: 6,
+            symbol: 'USDCso',
+            source: 'solana',
+            address: 'inj12pwnhtv7yat2s30xuf4gdk9qm85v4j3e60dgvu',
+            tokenType: 'cw20',
+          },
         }),
       )
     })
@@ -91,11 +97,14 @@ describe('token meta retrieved properly for', () => {
     it('fetches correct decimal value for peggy format', () => {
       const tokenMeta = tokenFactory.toToken(USDCPeggyDenom) as Token
 
-      expect(tokenMeta).toStrictEqual(
+      expect(tokenMeta).toEqual(
         expect.objectContaining({
-          decimals: 6,
-          symbol: 'USDC',
-          tokenType: 'erc20',
+          erc20: {
+            address: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+            decimals: 6,
+            symbol: 'USDC',
+            tokenType: 'erc20',
+          },
         }),
       )
     })
