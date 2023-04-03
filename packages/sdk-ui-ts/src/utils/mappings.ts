5c662b761b60944b5a291c0e200c84f1b98fe3ee
KURASH <92112412+kuraassh@users.noreply.github.com>
2023-04-03 23:13:39 +0300

    add validator DOUBLETOP

--- packages/sdk-ui-ts/src/utils/mappings.ts
+++ packages/sdk-ui-ts/src/utils/mappings.ts
@@ -311,4 +311,6 @@ export const validatorAddressToPathMap = {
     'injvaloper175x4lvxpwmmwt5m76yqz0ace8dwkjyjjj7sxdj.webp',
     injvaloper175x4lvxpwmmwt5m76yqz0ace8dwkjyjjj7sxdj:
     'injvaloper175x4lvxpwmmwt5m76yqz0ace8dwkjyjjj7sxdj.webp'
+  injvalcons1n5yluh6k9zqv74jw35r5s5zy808xpzfqp2wm7g:
+    'injvaloper1n5yluh6k9zqv74jw35r5s5zy808xpzfq4ea8jf.webp'
 } as Record<string, string>
