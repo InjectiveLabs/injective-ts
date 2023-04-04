6ba86fa31512027e3c513eb3e65b7fd664e3d8cd
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2023-04-04 19:28:35 +0200

    chore: ethers version

--- packages/wallet-ts/src/utils/wallets/metamask/Metamask.ts
+++ packages/wallet-ts/src/utils/wallets/metamask/Metamask.ts
@@ -1,10 +1,10 @@
-import { Eip1193Provider, ethers } from 'ethers'
+import { ethers } from 'ethers'
 import detectEthereumProvider from '@metamask/detect-provider'
 import { WalletException } from '@injectivelabs/exceptions'
 
 export const getEthersProviderFromMetamask = async () => {
   try {
-    const provider = await detectEthereumProvider<Eip1193Provider>({
+    const provider = await detectEthereumProvider({
       mustBeMetaMask: true,
     })
 
@@ -12,7 +12,7 @@ export const getEthersProviderFromMetamask = async () => {
       throw new WalletException(new Error('Please install Metamask Extension'))
     }
 
-    return new ethers.BrowserProvider(provider)
+    return new ethers.providers.Web3Provider(provider)
   } catch (e) {
     throw new WalletException(new Error('Please install Metamask Extension'))
   }
