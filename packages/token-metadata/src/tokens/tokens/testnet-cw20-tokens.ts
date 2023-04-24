f485aa5274bf66db61f86b9bca4058d0a8b84853
thomasRalee <thomas.leera@gmail.com>
2023-04-24 18:14:40 +0800

    chore: update testnet cw20 tokens decimals setting

--- packages/token-metadata/src/tokens/tokens/testnet-cw20-tokens.ts
+++ packages/token-metadata/src/tokens/tokens/testnet-cw20-tokens.ts
@@ -13,14 +13,14 @@ export default {
     tokenType: TokenType.Cw20,
   },
   ATOM: {
-    decimals: 6,
+    decimals: 8,
     symbol: 'ATOM',
     source: Cw20TokenSource.Cosmos,
     address: 'atom',
     tokenType: TokenType.Cw20,
   },
   WETH: {
-    decimals: 18,
+    decimals: 8,
     symbol: 'WETH',
     source: Cw20TokenSource.Cosmos,
     address: 'weth',
