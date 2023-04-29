b51c687e5cba23bcee15db4a78234be501fae9fc
Shane K Moore <41407272+shane-moore@users.noreply.github.com>
2023-04-29 10:25:30 -0500

    chore: updateMetamaskNetwork helper function

--- packages/wallet-ts/src/utils/wallets/metamask/Metamask.ts
+++ packages/wallet-ts/src/utils/wallets/metamask/Metamask.ts
@@ -1,6 +1,8 @@
 import { ethers } from 'ethers'
 import detectEthereumProvider from '@metamask/detect-provider'
 import { WalletException } from '@injectivelabs/exceptions'
+import { EthereumChainId } from '@injectivelabs/ts-types'
+import { Eip1993ProviderWithMetamask } from '../../../strategies/wallet-strategy/types'
 
 export const getEthersProviderFromMetamask = async () => {
   try {
@@ -13,6 +15,27 @@ export const getEthersProviderFromMetamask = async () => {
     }
 
     return new ethers.providers.Web3Provider(provider)
+  } catch (e) {
+    throw new WalletException(new Error('Please install Metamask Extension'))
+  }
+}
+
+export const updateMetamaskNetwork = async (chainId: EthereumChainId) => {
+  try {
+    const provider = (await detectEthereumProvider({
+      mustBeMetaMask: true,
+    })) as Eip1993ProviderWithMetamask
+
+    if (!provider) {
+      throw new WalletException(new Error('Please install Metamask Extension'))
+    }
+
+    const chainIDToHex = chainId.toString(16)
+
+    return await provider.request({
+      method: 'wallet_switchEthereumChain',
+      params: [{ chainId: `0x${chainIDToHex}` }],
+    })
   } catch (e) {
     throw new WalletException(new Error('Please install Metamask Extension'))
   }
