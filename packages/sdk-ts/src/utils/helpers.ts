0ec0d6da377eab1a19a5db93e92540721f9f7701
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2023-05-11 19:05:03 +0200

    chore: minor

--- packages/sdk-ts/src/utils/helpers.ts
+++ packages/sdk-ts/src/utils/helpers.ts
@@ -154,19 +154,3 @@ export const getErrorMessage = (error: any, endpoint: string): string => {
     ? error.response.data.message || error.response.data
     : error.response.statusText
 }
-
-export const formatNotificationDescription = (description: string) => {
-  const DESCRIPTION_CHARACTER_LIMIT = 60
-
-  if (description.length <= DESCRIPTION_CHARACTER_LIMIT) {
-    return {
-      description,
-      tooltip: '',
-    }
-  }
-
-  return {
-    description: description.slice(0, DESCRIPTION_CHARACTER_LIMIT) + ' ...',
-    tooltip: description,
-  }
-}
