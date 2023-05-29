d98140215e3212be0f2b6fb2bc427988e6bb308d
thomasRalee <thomas.leera@gmail.com>
2023-05-29 14:31:30 +0200

    refactor: devnet and testnet token meta overriding implementation

--- packages/token-metadata/src/tokens/network/devnet.ts
+++ packages/token-metadata/src/tokens/network/devnet.ts
@@ -1,49 +1,18 @@
-import { TokenMeta, Erc20TokenMeta } from '../../types'
-import tokens from '../tokens'
-
-export const devnetSymbolToAddressMap = {
-  INJ: '0xBe8d71D26525440A03311cc7fa372262c5354A3c',
-}
-
-export const devnet1SymbolToAddressMap = {
-  INJ: '0xBe8d71D26525440A03311cc7fa372262c5354A3c',
-}
-
-export const devnet2SymbolToAddressMap = {
-  INJ: '0xBe8d71D26525440A03311cc7fa372262c5354A3c',
-}
-
-const formatTokenMeta = (
-  symbol: string,
-  erc20AddressFromMap?: string,
-): TokenMeta => {
-  const tokenMeta = tokens[symbol]
-
-  if (!tokens[symbol].erc20 || !erc20AddressFromMap) {
-    return tokenMeta
-  }
-
-  return {
-    ...tokenMeta,
-    erc20: {
-      ...(tokens[symbol].erc20 as Erc20TokenMeta),
-      ...(erc20AddressFromMap ? { address: erc20AddressFromMap } : {}),
-    },
-  }
-}
+import { TokenMeta } from '../../types'
+import tokens, { devnetTokens } from '../tokens'
 
 export const getTokensBySymbolForDevnet = () =>
   (Object.keys(tokens) as Array<keyof typeof tokens>).reduce(
     (result, symbol) => {
-      const tokenSymbol = symbol as keyof typeof devnetSymbolToAddressMap
-      const erc20AddressFromMap = devnetSymbolToAddressMap[tokenSymbol]
+      const tokenSymbolToUpperCase = symbol.toUpperCase()
+      const tokenMeta =
+        devnetTokens[tokenSymbolToUpperCase] || tokens[tokenSymbolToUpperCase]
+
+      console.log({ tokenMeta })
 
       return {
         ...result,
-        [tokenSymbol.toUpperCase()]: formatTokenMeta(
-          symbol,
-          erc20AddressFromMap,
-        ),
+        [tokenSymbolToUpperCase]: tokenMeta,
       }
     },
     {},
@@ -52,15 +21,13 @@ export const getTokensBySymbolForDevnet = () =>
 export const getTokensBySymbolForDevnet1 = () =>
   (Object.keys(tokens) as Array<keyof typeof tokens>).reduce(
     (result, symbol) => {
-      const tokenSymbol = symbol as keyof typeof devnet1SymbolToAddressMap
-      const erc20AddressFromMap = devnet1SymbolToAddressMap[tokenSymbol]
+      const tokenSymbolToUpperCase = symbol.toUpperCase()
+      const tokenMeta =
+        devnetTokens[tokenSymbolToUpperCase] || tokens[tokenSymbolToUpperCase]
 
       return {
         ...result,
-        [tokenSymbol.toUpperCase()]: formatTokenMeta(
-          symbol,
-          erc20AddressFromMap,
-        ),
+        [tokenSymbolToUpperCase]: tokenMeta,
       }
     },
     {},
@@ -69,15 +36,13 @@ export const getTokensBySymbolForDevnet1 = () =>
 export const getTokensBySymbolForDevnet2 = () =>
   (Object.keys(tokens) as Array<keyof typeof tokens>).reduce(
     (result, symbol) => {
-      const tokenSymbol = symbol as keyof typeof devnet2SymbolToAddressMap
-      const erc20AddressFromMap = devnet2SymbolToAddressMap[tokenSymbol]
+      const tokenSymbolToUpperCase = symbol.toUpperCase()
+      const tokenMeta =
+        devnetTokens[tokenSymbolToUpperCase] || tokens[tokenSymbolToUpperCase]
 
       return {
         ...result,
-        [tokenSymbol.toUpperCase()]: formatTokenMeta(
-          symbol,
-          erc20AddressFromMap,
-        ),
+        [tokenSymbolToUpperCase]: tokenMeta,
       }
     },
     {},
