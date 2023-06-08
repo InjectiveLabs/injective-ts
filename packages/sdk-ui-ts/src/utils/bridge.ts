1ce1d51706c0ffee702194b437ff6bcfca0bb89a
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2023-06-09 00:19:32 +0200

    refactor: minor

--- packages/sdk-ui-ts/src/utils/bridge.ts
+++ packages/sdk-ui-ts/src/utils/bridge.ts
@@ -3,7 +3,6 @@ import {
   BridgeTransactionState,
   BridgingNetwork,
 } from './../types/bridge'
-import { convertTimestampToMilliseconds } from '@injectivelabs/utils'
 import {
   isTestnet,
   isMainnet,
@@ -399,99 +398,6 @@ export const ibcHashToNativeInjPerCosmosChain = {
   [CosmosChainId.Stride]:
     'ibc/A7454562FF29FE068F42F9DE4805ABEF54F599D1720B345D6518D9B5C64EA6D2',
 } as Partial<Record<CosmosChainId, string>>
-
-const sameTxHash = (txHashOne: string, txHashTwo: string) =>
-  txHashOne &&
-  txHashTwo &&
-  txHashOne.replace('0x', '').toLowerCase() ===
-    txHashTwo.replace('0x', '').toLowerCase()
-
-export const getCachedIBCTransactionState = (
-  transaction: UiBridgeTransaction,
-): BridgeTransactionState => {
-  if (
-    transaction.timeoutTimestamp &&
-    transaction.state === BridgeTransactionState.Submitted
-  ) {
-    const now = Date.now()
-    const bufferTime = 60 * 1000 // hardcode to 1 minute
-    const timeoutTimestampWithBuffer =
-      convertTimestampToMilliseconds(
-        parseInt(transaction.timeoutTimestamp, 10),
-      ) + bufferTime
-
-    if (now >= timeoutTimestampWithBuffer) {
-      return BridgeTransactionState.Failed
-    }
-
-    return BridgeTransactionState.Submitted
-  }
-
-  return transaction.state as BridgeTransactionState
-}
-
-export const findEthereumTransactionByNonce = (
-  transaction: UiBridgeTransaction,
-  comparingTransaction: UiBridgeTransaction,
-) =>
-  transaction.nonce &&
-  comparingTransaction.nonce &&
-  transaction.nonce === comparingTransaction.nonce
-
-export const findEthereumTransactionByTxHash = (
-  transaction: UiBridgeTransaction,
-  comparingTransaction: UiBridgeTransaction,
-) =>
-  transaction.txHash &&
-  comparingTransaction.txHash &&
-  sameTxHash(transaction.txHash, comparingTransaction.txHash)
-
-export const findEthereumTransactionByTxHashes = (
-  transaction: UiBridgeTransaction,
-  comparingTransaction: UiBridgeTransaction,
-) =>
-  transaction.txHashes &&
-  transaction.txHashes.find((hash: string) =>
-    sameTxHash(hash, comparingTransaction.txHash),
-  ) !== undefined
-
-export const findIBCTransactionByTimeoutTimestamp = (
-  transaction: UiBridgeTransaction,
-  comparingTransaction: UiBridgeTransaction,
-) =>
-  transaction.sender === comparingTransaction.sender &&
-  transaction.receiver === comparingTransaction.receiver &&
-  transaction.timeoutTimestamp &&
-  comparingTransaction.timeoutTimestamp &&
-  convertTimestampToMilliseconds(transaction.timeoutTimestamp) ===
-    convertTimestampToMilliseconds(comparingTransaction.timeoutTimestamp)
-
-export const ibcTxNotPartOfInjectiveIbcTxs =
-  (injectiveIbcTransactions: UiBridgeTransaction[]) =>
-  (transaction: UiBridgeTransaction) =>
-    injectiveIbcTransactions.find((injectiveIbcTransaction) =>
-      findIBCTransactionByTimeoutTimestamp(
-        injectiveIbcTransaction,
-        transaction,
-      ),
-    ) === undefined
-
-export const txNotPartOfPeggoDeposit =
-  (peggoDepositTxs: UiBridgeTransaction[]) =>
-  (transaction: UiBridgeTransaction) =>
-    peggoDepositTxs.find((peggoDepositTx) =>
-      findEthereumTransactionByTxHash(peggoDepositTx, transaction),
-    ) === undefined
-
-export const txNotPartOfInjectivePeggyTxs =
-  (injectivePeggyTransactions: UiBridgeTransaction[]) =>
-  (transaction: UiBridgeTransaction) =>
-    injectivePeggyTransactions.find(
-      (injectiveTransaction) =>
-        findEthereumTransactionByNonce(injectiveTransaction, transaction) ||
-        findEthereumTransactionByTxHashes(injectiveTransaction, transaction) ||
-        findEthereumTransactionByTxHash(injectiveTransaction, transaction),
-    ) === undefined
 
 export const getExplorerUrl = (network: Network): string => {
   if (isDevnet(network)) {
