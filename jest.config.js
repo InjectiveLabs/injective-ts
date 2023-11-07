090cab5aa22b2224f6717b0ad596088132ee0db9
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2023-11-07 11:40:45 +0100

    fix: import

--- jest.config.js
+++ jest.config.js
@@ -175,10 +175,7 @@ module.exports = {
   },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  transformIgnorePatterns: [
-    '^.+\\.json$',
-    'node_modules/(?!(@bangjelkoski/eth-crypto)/)',
-  ],
+  transformIgnorePatterns: ['^.+\\.json$', 'node_modules/(?!(eth-crypto)/)'],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
