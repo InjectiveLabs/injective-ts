6e25b3f156d964666db8bc7885df653166aac523
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2023-11-07 16:01:00 +0100

    revert: faulty version

--- packages/sdk-ts/src/utils/crypto.ts
+++ packages/sdk-ts/src/utils/crypto.ts
@@ -58,3 +58,27 @@ export const messageHash = (message: any) =>
     message.types,
     SignTypedDataVersion.V4,
   )
+
+export function uint8ArrayToHex(arr: Uint8Array) {
+  return Buffer.from(arr).toString('hex')
+}
+
+export function hexToUnit8Array(str: string) {
+  return new Uint8Array(Buffer.from(str, 'hex'))
+}
+
+export function decompressPubKey(startsWith02Or03: string) {
+  // if already decompressed an not has trailing 04
+  const testBuffer = Buffer.from(startsWith02Or03, 'hex')
+
+  if (testBuffer.length === 64) startsWith02Or03 = '04' + startsWith02Or03
+
+  let decompressed = uint8ArrayToHex(
+    secp256k1.publicKeyConvert(hexToUnit8Array(startsWith02Or03), false),
+  )
+
+  // remove trailing 04
+  decompressed = decompressed.substring(2)
+
+  return decompressed
+}
