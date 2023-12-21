52a15323fe60c4151d6f248e105219109b49b1ec
alpha-bulls <154057794+alpha-bulls@users.noreply.github.com>
2023-12-21 16:29:11 +0100

    no message

--- packages/token-metadata/src/tokens/tokens/tokens.ts
+++ packages/token-metadata/src/tokens/tokens/tokens.ts
@@ -2392,4 +2392,13 @@ export default {
     coinGeckoId: '',
     tokenType: TokenType.TokenFactory,
   },
+  
+  BULLS: {
+    name: 'BULLS',
+    symbol: 'BULLS',
+    decimals: 6,
+    logo: 'bulls.png',
+    coinGeckoId: '',
+    tokenType: TokenType.TokenFactory,
+  },
 } as Record<string, TokenMeta>
