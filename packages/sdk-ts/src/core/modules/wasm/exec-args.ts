063551f55b9a9326a7741cd501612ffbcde4229e
Ivan Angelkoski <ivanangelkoski95@gmail.com>
2024-02-24 17:01:12 +0100

    feat: pgt init

--- packages/sdk-ts/src/core/modules/wasm/exec-args.ts
+++ packages/sdk-ts/src/core/modules/wasm/exec-args.ts
@@ -11,6 +11,7 @@ import ExecArgInitiateTransfer from './exec-args/ExecArgInitiateTransfer'
 import ExecArgIncreaseAllowance from './exec-args/ExecArgIncreaseAllowance'
 import ExecArgRemoveGridStrategy from './exec-args/ExecArgRemoveGridStrategy'
 import ExecArgCreateSpotGridStrategy from './exec-args/ExecArgCreateSpotGridStrategy'
+import ExecArgCreatePerpGridStrategy from './exec-args/ExecArgCreatePerpGridStrategy'
 
 import ExecArgCW20AdapterRedeemAndTransfer from './exec-args/ExecArgCW20AdapterRedeemAndTransfer'
 
@@ -33,6 +34,7 @@ export type ExecArgs =
   | ExecArgIncreaseAllowance
   | ExecArgRemoveGridStrategy
   | ExecArgCreateSpotGridStrategy
+  | ExecArgCreatePerpGridStrategy
   | ExecArgCW20AdapterRedeemAndTransfer
 
 export type ExecPrivilegedArgs =
