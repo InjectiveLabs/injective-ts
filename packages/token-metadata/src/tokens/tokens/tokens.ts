9e50f8230dd55a7cc5dc6e080308bf1bcd3480b1
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2024-03-11 15:44:30 +0100

    chore: pyth legacy

--- packages/token-metadata/src/tokens/tokens/tokens.ts
+++ packages/token-metadata/src/tokens/tokens/tokens.ts
@@ -1983,7 +1983,7 @@ export default {
     cw20s: [
       {
         decimals: 6,
-        symbol: 'PYTH',
+        symbol: 'PYTHlegacy',
         source: TokenSource.Solana,
         address: 'inj1tjcf9497fwmrnk22jfu5hsdq82qshga54ajvzy',
       },
@@ -1991,7 +1991,7 @@ export default {
 
     ibcs: [
       {
-        symbol: 'PYTHgw',
+        symbol: 'PYTH',
         decimals: 6,
         isNative: false,
         baseDenom:
