547949fff451bc97d100519a96c5205fe7871ab4
Catripilar <70659123+catripilar@users.noreply.github.com>
2024-03-25 13:26:11 -0300

    Update cw20-tokens.ts

--- packages/token-metadata/src/tokens/tokens/cw20-tokens.ts
+++ packages/token-metadata/src/tokens/tokens/cw20-tokens.ts
@@ -143,4 +143,18 @@ export default {
       },
     ],
   },
+
+  KIRA: {
+    name: 'KiraTestToken',
+    logo: 'kira.jpeg',
+    coinGeckoId: '',
+
+    tokenFactories: [
+      {
+        decimals: 6,
+        symbol: 'KIRA',
+        creator: 'inj1jfuyujpvvkxq4566r3z3tv3jdy29pqra5ln0yk',
+      },
+    ],
+  },
 } as Record<string, TokenMetaBase>
