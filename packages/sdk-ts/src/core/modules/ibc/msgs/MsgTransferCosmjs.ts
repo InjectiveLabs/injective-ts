b008080eccee963b2a359145d257363b3ca19434
thomasRalee <thomas.leera@gmail.com>
2024-07-01 09:14:10 +0800

    chore: updae MsgTransferCosmjs

--- packages/sdk-ts/src/core/modules/ibc/msgs/MsgTransferCosmjs.ts
+++ packages/sdk-ts/src/core/modules/ibc/msgs/MsgTransferCosmjs.ts
@@ -1,4 +1,5 @@
-import { MsgTransferEncodeObject } from '@cosmjs/stargate'
+// import { MsgTransferEncodeObject } from '@cosmjs/stargate'
+import snakecaseKeys, { SnakeCaseKeys } from 'snakecase-keys'
 import { MsgTransfer as BaseMsgTransferCosmjs } from 'cosmjs-types/ibc/applications/transfer/v1/tx'
 import {
   CosmosBaseV1Beta1Coin,
@@ -76,27 +77,17 @@ export default class MsgTransferCosmjs {
   }
 
   public toAmino() {
-    const { params } = this
-
-    const transferMsg: MsgTransferEncodeObject = {
-      typeUrl: '/ibc.applications.transfer.v1.MsgTransfer',
-      value: BaseMsgTransferCosmjs.fromPartial({
-        sourcePort: params.port,
-        sourceChannel: params.channelId,
-        sender: params.sender,
-        receiver: params.receiver,
-        token: params.amount,
-        timeoutHeight: params.height
-          ? {
-              revisionHeight: BigInt(params.height.revisionHeight),
-              revisionNumber: BigInt(params.height.revisionNumber),
-            }
-          : undefined,
-        timeoutTimestamp: params.timeout ? BigInt(params.timeout) : undefined,
-      }),
+    const proto = this.toProto()
+    const message = {
+      ...snakecaseKeys(proto),
     }
 
-    return transferMsg
+    return {
+      type: 'cosmos-sdk/MsgTransfer',
+      value: {
+        ...message,
+      } as unknown as SnakeCaseKeys<IbcApplicationsTransferV1Tx.MsgTransfer>,
+    }
   }
 
   public toWeb3() {
