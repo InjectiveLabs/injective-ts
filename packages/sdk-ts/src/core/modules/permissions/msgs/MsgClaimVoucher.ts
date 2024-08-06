9efc4c9a59ba108022b15baea13fb6b968ef32b2
Juan-LukeKlopper <jlklopper@protonmail.com>
2024-08-06 22:33:45 +0200

    fix: update proto definition in MsgClaimVoucher

--- packages/sdk-ts/src/core/modules/permissions/msgs/MsgClaimVoucher.ts
+++ packages/sdk-ts/src/core/modules/permissions/msgs/MsgClaimVoucher.ts
@@ -1,26 +1,15 @@
 import { MsgBase } from '../../MsgBase';
 import snakecaseKeys from 'snakecase-keys';
-
-// future imports
-
-// import {
-//   InjectivePermissionsV1Beta1Tx
-// } from '@injectivelabs/care-proto-ts'
+import { InjectivePermissionsV1Beta1Tx } from '@injectivelabs/core-proto-ts';
 
 export declare namespace MsgClaimVoucher {
   export interface Params {
-    sender: string;
-    originator: string;
+    sender: string
+    denom: string
   }
 
-  // Temporary Proto declaration
-  export type Proto = {
-    sender: string;
-    originator: string;
-  };
 
-  // future Proto declaration
-  // export type Proto = InjectivePermissionsV1Beta1Tx.MsgClaimVoucher
+  export type Proto = InjectivePermissionsV1Beta1Tx.MsgClaimVoucher
 }
 
 /**
@@ -30,68 +19,67 @@ export default class MsgClaimVoucher extends MsgBase<
   MsgClaimVoucher.Params,
   MsgClaimVoucher.Proto
 > {
-  static fromJSON(params: MsgClaimVoucher.Params): MsgClaimVoucher {
-    return new MsgClaimVoucher(params);
+  static fromJSON(
+    params: MsgClaimVoucher.Params,
+  ): MsgClaimVoucher {
+    return new MsgClaimVoucher(params)
   }
 
   public toProto() {
-    const { params } = this;
+    const { params } = this
 
-    const message = {
-      sender: params.sender,
-      originator: params.originator,
-    };
+    const message = InjectivePermissionsV1Beta1Tx.MsgClaimVoucher.create()
+    message.sender = params.sender
+    message.denom = params.denom
 
-    return message;
+    return InjectivePermissionsV1Beta1Tx.MsgClaimVoucher.fromPartial(
+      message,
+    )
   }
 
   public toData() {
-    const proto = this.toProto();
+    const proto = this.toProto()
 
     return {
       '@type': '/injective.permissions.v1beta1.MsgClaimVoucher',
       ...proto,
-    };
+    }
   }
 
   public toAmino() {
-    const proto = this.toProto();
+    const proto = this.toProto()
     const message = {
       ...snakecaseKeys(proto),
-    };
+    }
 
     return {
       type: 'permissions/MsgClaimVoucher',
       value: message,
-    };
+    }
   }
 
   public toWeb3() {
     const amino = this.toAmino();
-    const { value } = amino;
+    const { value } = amino
 
     return {
       '@type': '/injective.permissions.v1beta1.MsgClaimVoucher',
       ...value,
-    };
+    }
   }
 
   public toDirectSign() {
-    const proto = this.toProto();
+    const proto = this.toProto()
 
     return {
       type: '/injective.permissions.v1beta1.MsgClaimVoucher',
       message: proto,
-    };
+    }
   }
 
   public toBinary(): Uint8Array {
-    // Placeholder for binary encoding
-    return new Uint8Array();
-
-    //// Expected binary encoding
-    // return InjectivePermissionsV1Beta1Tx.MsgClaimVoucher.encode()
-    //   this.toProto(),
-    // ).finish()
+    return InjectivePermissionsV1Beta1Tx.MsgClaimVoucher.encode(
+      this.toProto(),
+    ).finish()
   }
 }
