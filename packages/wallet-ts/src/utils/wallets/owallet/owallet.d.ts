740e6efefef1c6ee5281b8f04ee0020e296cebbe
toan <toan.dq@orai.io>
2024-10-29 14:10:41 +0700

    add owallet interface for global

--- packages/wallet-ts/src/utils/wallets/owallet/owallet.d.ts
+++ packages/wallet-ts/src/utils/wallets/owallet/owallet.d.ts
@@ -2,5 +2,8 @@ import type { Window as OWalletWindow } from '@owallet/types'
 
 declare global {
   // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  interface Window extends OWalletWindow {}
+  interface Window extends OWalletWindow {
+    owallet: OWalletWindow['owallet']
+  }
 }
+
