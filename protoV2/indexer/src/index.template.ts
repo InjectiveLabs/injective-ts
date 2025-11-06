c40710467e834120d2fc0dba198de31f61a53a7a
thomasRalee <thomas.leera@gmail.com>
2025-11-06 20:48:25 +0700

    feat: proto v2

---
+++ protoV2/indexer/src/index.template.ts
@@ -0,0 +1,42 @@
+// Auto-generated index file
+
+// Export all client classes
+export { EventProviderAPIClient } from "./generated/event_provider_api_pb.client";
+export { HealthClient } from "./generated/health_pb.client";
+export { InjectiveAccountsRPCClient } from "./generated/injective_accounts_rpc_pb.client";
+export { InjectiveArchiverRPCClient } from "./generated/injective_archiver_rpc_pb.client";
+export { InjectiveAuctionRPCClient } from "./generated/injective_auction_rpc_pb.client";
+export { InjectiveCampaignRPCClient } from "./generated/injective_campaign_rpc_pb.client";
+export { InjectiveChartRPCClient } from "./generated/injective_chart_rpc_pb.client";
+export { InjectiveDerivativeExchangeRPCClient } from "./generated/injective_derivative_exchange_rpc_pb.client";
+export { InjectiveExchangeRPCClient } from "./generated/injective_exchange_rpc_pb.client";
+export { InjectiveExplorerRPCClient } from "./generated/injective_explorer_rpc_pb.client";
+export { InjectiveInsuranceRPCClient } from "./generated/injective_insurance_rpc_pb.client";
+export { InjectiveMegavaultRPCClient } from "./generated/injective_megavault_rpc_pb.client";
+export { InjectiveMetaRPCClient } from "./generated/injective_meta_rpc_pb.client";
+export { InjectiveOracleRPCClient } from "./generated/injective_oracle_rpc_pb.client";
+export { InjectivePortfolioRPCClient } from "./generated/injective_portfolio_rpc_pb.client";
+export { InjectiveReferralRPCClient } from "./generated/injective_referral_rpc_pb.client";
+export { InjectiveSpotExchangeRPCClient } from "./generated/injective_spot_exchange_rpc_pb.client";
+export { InjectiveTradingRPCClient } from "./generated/injective_trading_rpc_pb.client";
+
+// Export all types as namespaces for easy import
+export * as EventProviderApiPb from "./generated/event_provider_api_pb";
+export * as HealthPb from "./generated/health_pb";
+export * as InjectiveAccountsRpcPb from "./generated/injective_accounts_rpc_pb";
+export * as InjectiveArchiverRpcPb from "./generated/injective_archiver_rpc_pb";
+export * as InjectiveAuctionRpcPb from "./generated/injective_auction_rpc_pb";
+export * as InjectiveCampaignRpcPb from "./generated/injective_campaign_rpc_pb";
+export * as InjectiveChartRpcPb from "./generated/injective_chart_rpc_pb";
+export * as InjectiveDerivativeExchangeRpcPb from "./generated/injective_derivative_exchange_rpc_pb";
+export * as InjectiveExchangeRpcPb from "./generated/injective_exchange_rpc_pb";
+export * as InjectiveExplorerRpcPb from "./generated/injective_explorer_rpc_pb";
+export * as InjectiveInsuranceRpcPb from "./generated/injective_insurance_rpc_pb";
+export * as InjectiveMegavaultRpcPb from "./generated/injective_megavault_rpc_pb";
+export * as InjectiveMetaRpcPb from "./generated/injective_meta_rpc_pb";
+export * as InjectiveOracleRpcPb from "./generated/injective_oracle_rpc_pb";
+export * as InjectivePortfolioRpcPb from "./generated/injective_portfolio_rpc_pb";
+export * as InjectiveReferralRpcPb from "./generated/injective_referral_rpc_pb";
+export * as InjectiveSpotExchangeRpcPb from "./generated/injective_spot_exchange_rpc_pb";
+export * as InjectiveTradingRpcPb from "./generated/injective_trading_rpc_pb";
+
