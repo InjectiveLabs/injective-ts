58b39a9f43a7a59ab2cefd28aa5d236334580753
thomasRalee <thomas.leera@gmail.com>
2025-11-06 20:48:25 +0700

    feat: proto v2

---
+++ protoV2/core/publish.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+cd ./protoV2/core/proto-ts
+
+v=$(npm view @injectivelabs/core-proto-ts-v2 version)
+echo "current package version: $v"
+
+v1="${v%.*}.$((${v##*.}+1))"
+echo "new package version: $v1"
+
+npm version $v1
+npm publish
