f800906deaedc5db93abf7ab9d17b1e788102ad6
Ivan Angelkoski <ivanangelkoski95@gmail.com>
2025-12-02 18:09:38 +0100

    feat: trezor eip1193 provider

--- packages/wallets/wallet-trezor/src/strategy/Base.ts
+++ packages/wallets/wallet-trezor/src/strategy/Base.ts
@@ -25,6 +25,7 @@ import {
 import { loadTrezorConnect } from './lib.js'
 import { transformTypedData } from '../utils.js'
 import { BaseTrezorTransport } from './hw/index.js'
+import { TrezorEip1193Provider } from './Eip1193Provider.js'
 import type { Hash, PublicClient } from 'viem'
 import type { EvmChainId } from '@injectivelabs/ts-types'
 import type { TxResponse } from '@injectivelabs/sdk-ts/core/tx'
@@ -39,6 +40,7 @@ import type {
 } from '@injectivelabs/sdk-ts/types'
 import type {
   StdSignDoc,
+  Eip1193Provider,
   SendTransactionOptions,
   ConcreteWalletStrategy,
   WalletStrategyEvmOptions,
@@ -462,6 +464,13 @@ export default class TrezorBase
     return (await accountManager.getWalletForAddress(
       address,
     )) as TrezorWalletInfo
+  }
+
+  public async getEip1193Provider(): Promise<Eip1193Provider> {
+    return new TrezorEip1193Provider(this.trezor, {
+      chainId: this.evmOptions.evmChainId.toString(),
+      derivationPath: this.baseDerivationPath,
+    })
   }
 
   private async getPublicClient(evmChainId?: EvmChainIdType) {
